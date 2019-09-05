57aca50d96c40621524451ef9945b19743198aee
Neil Vaytet <neil.vaytet@esss.se>
2019-09-05 14:11:22 +0200

    added script for x vs tof

---
+++ src/v20dataviz/__init__.py
@@ -0,0 +1,2 @@
+from .image import image
+from .x_vs_tof import x_vs_tof
--- src/v20dataviz/x_vs_tof.py
+++ src/v20dataviz/x_vs_tof.py
@@ -3,39 +3,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def image(filename,
-          entry="/entry/event_data/event_id",
+def x_vs_tof(filename,
+          id_path="/entry/event_data/event_id",
+          tof_path="/entry/event_data/event_time_offset",
           colormap="viridis",
           vmin=None,
           vmax=None,
           log=False,
+          nbins=512,
           save=None):
     """
-    Make a 2D image of the detector counts
+    Make a x vs tof image
     """
 
     with h5py.File(filename, "r") as f:
 
-        ids = np.array(f[entry][...], dtype=np.int32, copy=True)
+        ids = np.array(f[id_path][...], dtype=np.int32, copy=True)
+        tofs = np.array(f[tof_path][...], dtype=np.float64, copy=True) / 1.0e3
         x = np.array(f["/entry/instrument/detector_1/x_pixel_offset"][...],
                      dtype=np.float64, copy=True)
         y = np.array(f["/entry/instrument/detector_1/y_pixel_offset"][...],
                      dtype=np.float64, copy=True)
         nx, ny = np.shape(x)
-        z, edges = np.histogram(ids, bins=np.arange(nx * ny + 1))
-        z = z.reshape(nx, ny)
+        t = np.linspace(0.0, 7.2e4, nbins + 1)
+        z, xe, ye = np.histogram2d(ids, tofs, bins=[np.arange(nx * ny + 1), t])
+        z = z.reshape(nx, ny, nbins)
+        z = np.sum(z, axis=1)
         if log:
             z = np.log10(z)
 
         fig = plt.figure()
         ax = fig.add_subplot(111)
-        im = ax.imshow(z, origin="lower", aspect="equal", interpolation="none",
+        im = ax.imshow(z, origin="lower", aspect="auto", interpolation="none",
                        vmin=vmin, vmax=vmax,
-                       extent=[x[0, 0], x[0, -1], y[0, 0], y[-1, 0]])
+                       extent=[t[0], t[-1], x[0, 0], x[0, -1]])
         cb = plt.colorbar(im, ax=ax)
         cb.ax.set_ylabel("Counts")
-        ax.set_xlabel("x position [m]")
-        ax.set_ylabel("y position [m]")
+        ax.set_xlabel("Time-of-flight [microseconds]")
+        ax.set_ylabel("x position [m]")
         if save is not None:
             extension = "." + f.split(".")[-1]
             fig.savefig(f.replace(extension, ".pdf"), bbox_inches="tight")
