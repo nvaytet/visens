1358cf3c70a340432d1e74bf1a6bd8fadc58a5af
Neil Vaytet <neil.vaytet@esss.se>
2019-09-12 16:08:01 +0200

    changed directory name. include setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visens",
+    version="0.0.3",
+    author="Neil Vaytet",
+    author_email="neil.vaytet@esss.se",
+    description="VISualization for Ess Neutron Science",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nvaytet/visens",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "h5py",
+        "ipyvolume",
+    ],
+)
