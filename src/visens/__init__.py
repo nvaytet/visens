7d34ab0519d3ca7c7da0568455107b04d6f3e52f
Neil Vaytet <neil.vaytet@esss.se>
2019-09-23 22:27:31 +0200

    added y_vs_tof script

--- src/visens/__init__.py
+++ src/visens/__init__.py
@@ -1,6 +1,7 @@
 from .tof import tof
 from .image import image
 from .x_vs_tof import x_vs_tof
+from .y_vs_tof import y_vs_tof
 from .slicer import slicer
 from .volume import volume
 from .instrument_view import instrument_view
---
+++ src/visens/y_vs_tof.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .load import load
+
+
+def y_vs_tof(filename, colormap="viridis", vmin=None, vmax=None, log=False,
+             nbins=512, save=None, transpose=False, **kwargs):
+    """
+    Make a y vs tof image
+    """
+
+    data = load(filename, ids=True, tofs=True, **kwargs)
+
+    t = np.linspace(0.0, 7.2e4, nbins + 1)
+    z, xe, ye = np.histogram2d(data.ids, data.tofs/1.0e3,
+                               bins=[np.arange(-0.5, data.nx * data.ny + 0.5),
+                                     t])
+    z = z.reshape(data.nx, data.ny, nbins)
+    z = np.sum(z, axis=int(not transpose))
+    clab = "Counts"
+    if log:
+        with np.errstate(divide="ignore", invalid="ignore"):
+            z = np.log10(z)
+        clab = "log({})".format(clab)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(z, origin="lower", aspect="auto", interpolation="none",
+                   vmin=vmin, vmax=vmax,
+                   extent=[t[0], t[-1], data.x[0, 0], data.x[0, -1]])
+    cb = plt.colorbar(im, ax=ax)
+    cb.ax.set_ylabel(clab)
+    ax.set_xlabel("Time-of-flight [microseconds]")
+    ax.set_ylabel("y position [m]")
+    ax.set_title(filename.split("/")[-1])
+    if save is not None:
+        fig.savefig(save, bbox_inches="tight")
+    else:
+        plt.show()
