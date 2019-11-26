3d6bf24344ac24cc79364562cf6972aaf0e60fbf
Neil Vaytet <neil.vaytet@esss.se>
2019-11-26 16:58:10 +0100

    log and version

--- src/visens/preview.py
+++ src/visens/preview.py
@@ -6,21 +6,21 @@ from .tof import tof
 from .xy_vs_tof import x_vs_tof, y_vs_tof
 
 
-def preview(filename=None, data=None, save=None, **kwargs):
+def preview(filename=None, data=None, save=None, log=False):
     """
     Make a 4-panel file preview
     """
 
     if data is None:
-        data = load(filename, ids=True, tofs=True, convert_ids=False, **kwargs)
+        data = load(filename, ids=True, tofs=True, convert_ids=False)
 
     fig, ax = plt.subplots(2, 2, figsize=(12, 9))
     fig.suptitle(filename.split("/")[-1])
 
-    tof(data=data, ax=ax[0][0], **kwargs)
-    x_vs_tof(data=data, ax=ax[0][1], log=True, **kwargs)
-    image(data=data, ax=ax[1][0], side_panels=False, log=True, **kwargs)
-    y_vs_tof(data=data, ax=ax[1][1], log=True, **kwargs)
+    tof(data=data, ax=ax[0][0])
+    x_vs_tof(data=data, ax=ax[0][1], log=log)
+    image(data=data, ax=ax[1][0], side_panels=False, log=log)
+    y_vs_tof(data=data, ax=ax[1][1], log=log)
 
     if save is not None:
         fig.savefig(save, bbox_inches="tight")
